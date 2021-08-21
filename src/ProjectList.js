9a223c43b5153deb8771b3f6eb6159a4ff5ae4cd
Felipe Buso <felipe_buso@hotmail.com>
2021-08-21 19:27:20 -0300

    adicionado starwars planet search

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,5 +1,11 @@
 const projetos = [
   {
+    nome: 'Starwars Planet Search',
+    linkVideo: 'https://www.youtube.com/embed/aHA9o0Rg4GY?controls=0"',
+    linkProjeto: 'https://FelipeBuso.github.io/starwars-planets-search',
+    descricao: 'Lista com filtros de planetas do universo de Star Wars usando Context API e Hooks para controlar os estados globais.'
+  },
+  {
     nome: 'Trybewallet',
     linkVideo: 'https://www.youtube.com/embed/G9oEgL2q5ss?controls=0',
     linkProjeto: 'https://FelipeBuso.github.io/trybewallet',
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import CardProject from '../components/CardProject';
 import Header from '../components/Header';
 import projetos from '../ProjectList';
-// import { Link } from 'react-router-dom';
+import '../styles/Projects.css';
 
 export default function Projects() {
   return (
-    <div>
+    <div className="projects-section">
       <Header />
-      { projetos.map((project, i) => (
-        <CardProject key={ i } project={ project } />
-      ))}
-      {/* <h3>Trybewallet</h3>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/G9oEgL2q5ss?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <a href="https://FelipeBuso.github.io/trybewallet">Ver projeto</a> */}
+      <div className="project-cards-div">
+        { projetos.map((project, i) => (
+          <CardProject key={ i } project={ project } />
+        ))}
+      </div>
     </div>
   )
 }
