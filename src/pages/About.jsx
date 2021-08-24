63e741e151549b9427b61e2352cbe604e43078c0
Felipe Buso <felipe_buso@hotmail.com>
2021-08-23 22:01:27 -0300

    correçao layout responsivo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,11 @@ export default function About() {
   return (
     <div>
       <Header/>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <h1>Em construção</h1>
     </div>
   )
