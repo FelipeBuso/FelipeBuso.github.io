84d32ae6b5da2efdb386bfec9b0e380d0b905b94
Felipe Buso <felipe_buso@hotmail.com>
2021-08-23 22:07:35 -0300

    correçao componente about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import '../styles/About.css';
 
 export default function About() {
   return (
-    <div>
+    <div className="about-section">
       <Header/>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <h1>Em construção</h1>
+      <h1>Em atualização</h1>
+      <Footer />
     </div>
   )
 }
